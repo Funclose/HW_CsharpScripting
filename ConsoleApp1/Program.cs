06cfa1ee6a08a81d89967616baa96daac3c3734e
Funclose <ar.doroxoff@gmail.com>
2024-10-08 20:32:32 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Scripting;
+namespace ConsoleApp1
+{
+     class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("введите математическое выражение: ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var result = await CalculatedExpression(input);
+                    Console.WriteLine($"result :{result}");
+                }
+                 catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+            }
+        }
+
+        static async Task<double> CalculatedExpression(string exrp)
+        {
+            var option = ScriptOptions.Default
+                .AddReferences(typeof(Math).Assembly)
+                .AddImports("System", "System.Math");
+            return await CSharpScript.EvaluateAsync<double>(exrp, option);
+        }
+    }
+}
